d0bf9091e8522562f8a48759648114afd05903e5
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-11-13 02:12:27 -0500

    feat: implement weather plan-a-date service

--- client/src/services/plan-a-date/weather.js
+++ client/src/services/plan-a-date/weather.js
@@ -1,15 +1,31 @@
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const fetchData = async () => {
+  // TODO: get latitude and longitude of user dynamically
+  const URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=40.7934&longitude=-77.86&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1";
 
-const sampleData = {
-  Temp: "72°F",
-  Wind: "5 mph",
-  Humidity: "60%",
-  Forecast: "Sunny",
+  const res = await fetch(URL);
+  const data = await res.json();
+
+  const measurements = data["current"];
+  const units = data["current_units"];
+
+  const temp = measurements["temperature_2m"] + units["temperature_2m"];
+  const precipitation = measurements["precipitation"] + units["precipitation"];
+  const humidity =
+    measurements["relative_humidity_2m"] + units["relative_humidity_2m"];
+  const wind = measurements["wind_speed_10m"] + units["wind_speed_10m"];
+
+  return {
+    temp,
+    precipitation,
+    humidity,
+    wind,
+  };
 };
 
 export const weatherData = {
   title: "Weather Forecast",
   description: "Plan a date with the latest weather information.",
   buttonText: "Check Weather",
-  fetchData: () => sampleData,
+  fetchData,
 };
