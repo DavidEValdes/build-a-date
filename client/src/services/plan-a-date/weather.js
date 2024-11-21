70f521c7500f6e5705d9ceaa2d227141bce0081a
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-11-20 22:27:37 -0500

    update weather plan-a-date module to use user location

--- client/src/services/plan-a-date/weather.js
+++ client/src/services/plan-a-date/weather.js
@@ -1,9 +1,27 @@
 const fetchData = async () => {
-  // TODO: get latitude and longitude of user dynamically
-  const URL =
-    "https://api.open-meteo.com/v1/forecast?latitude=40.7934&longitude=-77.86&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1";
+  const getLocation = async () => {
+    if (navigator.geolocation) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            resolve({ latitude, longitude });
+          },
+          (error) => {
+            reject(error);
+          },
+        );
+      });
+    } else {
+      throw new Error("Geolocation is not supported by this browser.");
+    }
+  };
 
-  const res = await fetch(URL);
+  const { latitude, longitude } = await getLocation();
+
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`;
+
+  const res = await fetch(url);
   const data = await res.json();
 
   const measurements = data["current"];
@@ -16,10 +34,10 @@ const fetchData = async () => {
   const wind = measurements["wind_speed_10m"] + units["wind_speed_10m"];
 
   return {
-    temp,
-    precipitation,
-    humidity,
-    wind,
+    Temperature: temp,
+    Precipitation: precipitation,
+    Humidity: humidity,
+    Wind: wind,
   };
 };
 
